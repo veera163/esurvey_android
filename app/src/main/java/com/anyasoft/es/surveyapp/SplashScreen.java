f50a37f201f662df1ceb0cf8fb17cbe549a5fed1
sai <kiran.valluri@gmail.com>
2017-08-16 23:21:03 +0530

    permissions

--- app/src/main/java/com/anyasoft/es/surveyapp/SplashScreen.java
+++ app/src/main/java/com/anyasoft/es/surveyapp/SplashScreen.java
@@ -1,21 +1,20 @@
 package com.anyasoft.es.surveyapp;
 
-import android.content.DialogInterface;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v7.app.AlertDialog;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.anyasoft.es.surveyapp.location.GPSTracker;
-import com.anyasoft.es.surveyapp.logger.L;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -26,14 +25,24 @@ public class SplashScreen extends AppCompatActivity {
     private GPSTracker gps;
     private Location location;
 
+    Context context;
+    private int REQ_PERMISSIONS = 6;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        checkAndMove();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        context = this;
         imageView = (ImageView) findViewById(R.id.splash_icon);
 
-        Animation zoom	=	AnimationUtils.loadAnimation(SplashScreen.this, R.anim.splash_screen);
+        Animation zoom = AnimationUtils.loadAnimation(SplashScreen.this, R.anim.splash_screen);
 
         AnimationSet s = new AnimationSet(false);
 
@@ -45,12 +54,10 @@ public class SplashScreen extends AppCompatActivity {
             public void onAnimationStart(Animation animation) {
 
             }
+
             @Override
             public void onAnimationEnd(Animation animation) {
-               //checkAndMove();
-                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-                finish();
-                startActivity(intent);
+                checkAndMove();
             }
 
             @Override
@@ -59,6 +66,33 @@ public class SplashScreen extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void checkAndMove() {
+        if (requirePermissionCheck()) {
+            Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+            finish();
+            startActivity(intent);
+        } else {
+            askPermssion();
+        }
+    }
+
+    private void askPermssion() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE
+        }, REQ_PERMISSIONS);
+    }
+
+
+    private boolean requirePermissionCheck() {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
     }
 
 }
